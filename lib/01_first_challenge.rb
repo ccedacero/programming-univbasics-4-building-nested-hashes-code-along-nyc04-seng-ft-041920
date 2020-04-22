a08c6311997c22a6b74d195e95d772fce18390e9
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-22 13:28:51 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,12 +1,6 @@
 def first_challenge
  epic_tragedy = {
    :montague => {},
-   :capulet => {},
-}
+   :capulet => {}
+  }
 end
-
-
- expect(first_challenge.keys).to eq([:montague, :capulet])
-    expect(first_challenge.values).to eq([{}, {}])
-    
-    
